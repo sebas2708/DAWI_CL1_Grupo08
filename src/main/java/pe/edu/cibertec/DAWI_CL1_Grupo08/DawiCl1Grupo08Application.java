a4445ebba3154c3f3581ecc8b2b31cd2f15adf48
sebas2708 <sebasalon2702@gmail.com>
2024-04-11 21:59:36 -0500

    CL1_Grupo8v1

---
+++ src/main/java/pe/edu/cibertec/DAWI_CL1_Grupo08/DawiCl1Grupo08Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWI_CL1_Grupo08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl1Grupo08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl1Grupo08Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAWI_CL1_Grupo08/DawiCl1Grupo08ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWI_CL1_Grupo08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl1Grupo08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
