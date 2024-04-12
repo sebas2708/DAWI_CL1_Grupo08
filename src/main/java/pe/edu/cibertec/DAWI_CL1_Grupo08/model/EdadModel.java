eaeb34e0b8de26790a8d1387ad7dbef5f45a553f
sebas2708 <sebasalon2702@gmail.com>
2024-04-11 22:14:48 -0500

    Pregunta 2 del CL1 terminada

---
+++ src/main/java/pe/edu/cibertec/DAWI_CL1_Grupo08/model/EdadModel.java
@@ -0,0 +1,11 @@
+package pe.edu.cibertec.DAWI_CL1_Grupo08.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EdadModel {
+    private int edad1;
+    private int edad2;
+}
