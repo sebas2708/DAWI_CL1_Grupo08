c4678362bcb8619c39eb20c19d4c6e1682ac8374
Nexucbtffdd1 <i202213839@cibertec.edu.pe>
2024-04-12 19:09:51 -0500

    Pregunta 3 actualizada

--- src/main/java/pe/edu/cibertec/DAWI_CL1_Grupo08/controller/NumeroController.java
+++ src/main/java/pe/edu/cibertec/DAWI_CL1_Grupo08/controller/NumeroController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class NumeroController {
 
-    @GetMapping("/")
+    @GetMapping("/formulario")
     public String mostrarFormulario() {
         return "formulario";
     }
